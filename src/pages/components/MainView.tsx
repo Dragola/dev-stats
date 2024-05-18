a6a46787c31ca5e0a795e61c62536043434348c3
FadedBronze <TheSamyatGautam@gmail.com>
2024-05-18 12:12:44 -0400

    anims

---
+++ src/pages/components/MainView.tsx
@@ -0,0 +1,63 @@
+import { GeistSans } from "geist/font/sans";
+import { koulen } from "../_app";
+import { motion, useAnimate } from "framer-motion"
+import { useEffect } from "react";
+
+export type User = {
+	profileImage: string,
+	id: string,
+	commitCount: number,
+	repoCount: number,
+	websiteUrl?: string,
+	languages: [string, number][],
+	frameworks: [string, number][],
+}
+
+export default function MainView(props: User) {
+	const [scope, animate] = useAnimate()
+
+	useEffect(() => {
+		void animate("div", {opacity: ["0%", "100%"]}, {duration: 0.8});
+		void animate(".deez", {x: ["-100vw", "0"]}, {duration: 0.5});
+	}, [props.profileImage])
+
+	return (
+		<motion.div 
+			ref={scope}
+			className="gap-6 flex">
+			<div className="flex flex-col gap-6 deez">
+				<img
+					alt="profile"
+					src={props.profileImage} 
+					className="border border-gray-700 rounded-full w-80 h-80"
+				>
+				</img>
+				<div className="flex gap-4 justify-between">
+					<p className="text-white koulen text-2xl" style={koulen.style}>{props.id}</p>
+				</div>	
+			</div>
+			<motion.div className={"flex gap-4 text-white [&>div>h3]:text-2xl " + koulen.className}>
+				<div>
+					<h3>Languages:</h3>
+					{props.languages.map(([language]) => {
+						return <p key={language} style={GeistSans.style}>{language}</p>
+					})}
+				</div>
+				<div>
+					<h3>Frameworks {"&"} Libraries:</h3>
+					{props.frameworks.map(([framework]) => {
+						return <p key={framework} style={GeistSans.style}>{framework}</p>
+					})}	
+				</div>
+				<div>
+					<h3>General Stats</h3>
+					<p style={GeistSans.style}>Commits: {props.commitCount}</p>
+					<p style={GeistSans.style}>Total Repos: {props.repoCount}</p>
+					{props.websiteUrl && 
+						<p style={GeistSans.style}>Website Url: <a className="text-blue-400" href={`${props.websiteUrl}`}>{props.websiteUrl}</a></p>
+					}	
+				</div>
+			</motion.div>
+		</motion.div>
+	);
+}
---
+++ src/pages/components/Search.tsx
@@ -0,0 +1,26 @@
+import {motion} from "framer-motion"
+
+export default function SearchBar(props: {
+	users: {
+		id: string,
+	}[],
+	setActiveUser: (activeUserId: string) => void,
+	activeUser: string,
+}) {
+	return  (
+		<div className="flex flex-col flex gap-4 justify-center w-full">
+			<input placeholder="https://github.com/username" className="text-white rounded-full px-4 py-1 bg-gray-800 border border-gray-600"></input>
+			<div className="w-full flex gap-4">{props.users.map(({ id }) => {
+				return (<motion.button whileHover={{
+					backgroundColor: "#ffffff",
+					color: "#374151"
+				}} animate={{
+					backgroundColor: props.activeUser === id ? "#ffffff" : "#374151",
+					color: props.activeUser === id ?  "#374151" : "#ffffff"
+				}} onClick={() => props.setActiveUser(id)} className={
+					"rounded-full px-6 py-1" 
+				} key={id}>{id}</motion.button>)
+			})}</div>	
+		</div>
+	)
+}
