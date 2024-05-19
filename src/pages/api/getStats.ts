b1564d5c79f2123e93f5d0c47a227b5f93a41781
Bryan Gadd <x-dragola-x@live.ca>
2024-05-19 05:35:13 -0400

    Fixed response not being sent

--- src/pages/api/getStats.ts
+++ src/pages/api/getStats.ts
@@ -10,22 +10,22 @@ import { checkForRateLimit } from "~/server/checkForRateLimit";
 
 export default async function getStats(req: NextApiRequest, res: NextApiResponse) {
    const userUrlStr = req.query.userUrl;
-   if (userUrlStr === undefined || !isValidGithubURL(userUrlStr)) {
-      res.status(422);
-      return;
-   }
-
+   if (userUrlStr === undefined || !isValidGithubURL(userUrlStr)) return res.status(422);
+   
    const userUrl = new URL(userUrlStr as string);
    const user = userUrl.pathname.substring(1);
 
    const octokit = new Octokit({
       auth: process.env.GITHUB_AUTH,
    });
+
+   console.log('Before');
+
    const repoRes = await octokit.rest.repos.listForUser({
       username: user
    });
 
-   console.log(' Check if repoRes is rate limited!');
+   console.log('Check if repoRes is rate limited!');
 
    const checkAPI = checkForRateLimit(repoRes);
    if (checkAPI.rateLimited) return res.status(checkAPI.statusCode).json({rateLimit: checkAPI});
@@ -33,31 +33,31 @@ export default async function getStats(req: NextApiRequest, res: NextApiResponse
    console.log('checkAPI- not rate limited!');
 
    const commitStats = await getCommitStats(user, octokit);
-   if (commitStats === -1) return res.status(500);
+   if (commitStats === -1) return res.status(500).send('Bad data');
    else if (typeof commitStats === 'object') return res.status(commitStats.statusCode).json({rateLimit: commitStats});
 
    console.log('commitStats collected');
 
    const repoStats = await getRepoStats(user, repoRes.data, octokit);
-   if (!repoStats) return res.status(500);
+   if (!repoStats) return res.status(500).send('Bad data');
    else if ('timeToReset' in repoStats) return res.status(repoStats.statusCode).json({rateLimit: repoStats});
 
    console.log('repoStats collected');
 
    const languages = await getLanguages(user, repoRes.data, octokit);
-   if (!languages) return res.status(500);
+   if (!languages) return res.status(500).send('Bad data');
    else if ('timeToReset' in languages) return res.status(languages.statusCode).json({rateLimit: languages});
 
    console.log('languages collected');
 
    const frameworks = await getFrameworks(user, repoRes.data, octokit);
-   if (!frameworks) return res.status(500);
+   if (!frameworks) return res.status(500).send('Bad data');
    else if ('timeToReset' in frameworks) return res.status(frameworks.statusCode).json({rateLimit: frameworks});
 
    console.log('frameworks collected');
 
    const userStats = await getUserStats(user, octokit);
-   if (!userStats) return res.status(500);
+   if (!userStats) return res.status(500).send('Bad data');
    else if ('timeToReset' in userStats) return res.status(userStats.statusCode).json({ rateLimit: userStats});
 
    console.log('userStats collected');
