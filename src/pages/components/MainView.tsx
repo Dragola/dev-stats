8a9abbe31e5ee61d2d39be57abaf3d2d87c7655d
Cyronite <prasunsharma@icloud.com>
2024-05-19 03:46:14 -0400

    loading sppiner

--- src/pages/components/MainView.tsx
+++ src/pages/components/MainView.tsx
@@ -25,7 +25,7 @@ export default function MainView(props: User) {
 	return (
 		<motion.div 
 			ref={scope}
-			className="gap-12 border-white border-b flex flex-wrap justify-center">
+			className="gap-12 border-white pb-4  border-b flex flex-wrap justify-center">
 			<div className="flex flex-col gap-12 deez">
 				<img
 					alt="profile"
@@ -37,7 +37,7 @@ export default function MainView(props: User) {
 					<p className="text-white koulen text-3xl" style={koulen.style}>{props.id}</p>
 				</div>	
 			</div>
-			<motion.div className={"flex flex-wrap justify-around grow gap-6 text-white [&>div>h3]:text-2xl " + koulen.className}>
+			<motion.div className={"flex flex-wrap sm2:justify-around grow gap-6 text-white [&>div>h3]:text-2xl " + koulen.className}>
 				<div>
 					<h3>Languages:</h3>
 					{props.languages.map(([language]) => {
--- src/pages/components/Search.tsx
+++ src/pages/components/Search.tsx
@@ -1,58 +1,69 @@
 import { motion } from "framer-motion";
 import { type User } from "../components/MainView";
+import searchImage from "~/images/search.svg";
+import { useState } from "react";
 
 export default function SearchBar(props: {
 	users: User[],
 	setUsers: (newUsers: User[]) => void,
 	setActiveUser: (activeUserId: string) => void,
 	activeUser?: string,
+	setLoading: (loadingState: boolean) => void,
 }) {
+
 	return  (
 		<div className="flex flex-col gap-4 justify-center w-full">
-			<input onKeyUp={(e) => {
-				if (e.key !== "Enter") {
-					return;
-				}
-
-				const value = e.currentTarget.value;
-
-				fetch("api/getStats?userUrl=https://github.com/" + e.currentTarget.value, {
-					method: "GET",
-				}).then((res) => {
-					return res.json()
-				}).then((data: { 
-					commitStats: number, 
-					repoStats: { 
-						totalRepos: number
-					}, 
-					languages: Record<string, number>,
-					timestamp: string,
-					frameworks: Record<string, number>,
-					userStats: { avatar_url:string, blog?: string },
-				}) => {
-					console.log(data)
-
-					const thing: User = {
-						timeStamp: data.timestamp,
-						profileImage: data.userStats.avatar_url,
-						id: "@" + value,
-						commitCount: data.commitStats,
-						repoCount: data.repoStats.totalRepos,
-						websiteUrl: data.userStats.blog === "" ? undefined : data.userStats.blog,
-						languages: Object.keys(data.languages).map((language) => {
-							return [language,data.languages[language]!]
-						}),
-						packages: Object.keys(data.frameworks).map((framework) => {
-							return [framework,data.frameworks[framework]!]
-						})
+			<div className="relative w-full">
+				<img className="absolute top-1/2 -translate-y-1/2 right-2 scale-90" src={searchImage.src}></img>
+				<input className="w-full text-white rounded-full px-4 py-1 bg-gray-800 border border-gray-600" onKeyUp={(e) => {
+					if (e.key !== "Enter") {
+						return;
 					}
 
-					console.log(data, thing);
-					props.setUsers([...props.users, thing]);
-				}).catch((err) => {
-					console.error(err)	
-				})
-			}} placeholder="username" className="text-white rounded-full px-4 py-1 bg-gray-800 border border-gray-600"></input>
+					const value = e.currentTarget.value;
+
+					props.setLoading(true);
+
+					fetch("api/getStats?userUrl=https://github.com/" + e.currentTarget.value, {
+						method: "GET",
+					}).then((res) => {
+						return res.json()
+					}).then((data: { 
+						commitStats: number, 
+						repoStats: { 
+							totalRepos: number
+						}, 
+						languages: Record<string, number>,
+						timestamp: string,
+						frameworks: Record<string, number>,
+						userStats: { avatar_url:string, blog?: string },
+					}) => {
+						console.log(data)
+
+						const thing: User = {
+							timeStamp: data.timestamp,
+							profileImage: data.userStats.avatar_url,
+							id: "@" + value,
+							commitCount: data.commitStats,
+							repoCount: data.repoStats.totalRepos,
+							websiteUrl: data.userStats.blog === "" ? undefined : data.userStats.blog,
+							languages: Object.keys(data.languages).map((language) => {
+								return [language,data.languages[language]!]
+							}),
+							packages: Object.keys(data.frameworks).map((framework) => {
+								return [framework,data.frameworks[framework]!]
+							})
+						}
+
+						console.log(data, thing);
+						props.setUsers([...props.users, thing]);
+						props.setLoading(false);
+					}).catch((err) => {
+						console.error(err)	
+					})
+				}} placeholder="username">
+				</input>
+			</div>
 			<div className="w-full flex flex-wrap gap-4">{props.users.map(({ id, timeStamp }) => {
 				return (<motion.button whileHover={{
 					backgroundColor: props.activeUser && props.activeUser === id + timeStamp ? "#DB665D" : "#ffffff",
